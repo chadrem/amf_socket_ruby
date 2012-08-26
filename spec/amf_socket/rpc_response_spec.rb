1edc3ee0b1502cbd86a9ea33f6d02ba6ce53d83b
Chad Remesch <chad@remesch.com>
2012-08-26 16:44:13 -0400

    Spec for AmfSocket::RpcResponse

---
+++ spec/amf_socket/rpc_response_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe AmfSocket::RpcResponse do
+  it 'should construct properly' do
+    request = stub
+
+    response_object = {
+      :type => 'rpcResponse',
+      :response => {
+        :messageId => 'random id',
+        :result => 'foobar'
+      }
+    }
+
+    response = AmfSocket::RpcResponse.new(request, response_object)
+
+    response.request.should == request
+    response.result.should == 'foobar'
+  end
+end
