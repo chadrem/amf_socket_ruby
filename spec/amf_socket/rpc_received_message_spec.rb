3a66795d3fb982244a41052ea71bac0a213113e0
Chad Remesch <chad@remesch.com>
2012-08-26 16:52:14 -0400

    Spec for AmfSocket::RpcReceivedMessage

---
+++ spec/amf_socket/rpc_received_message_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe AmfSocket::RpcReceivedMessage do
+  it 'should construct properly' do
+    object = {
+      :type => 'rpcMessage',
+      :message => {
+        :messageId => 'random id',
+        :command => 'hello',
+        :params => {
+          :foo => 'bar'
+        }
+      }
+    }
+
+    conn = stub
+
+    message = AmfSocket::RpcReceivedMessage.new(object, conn)
+
+    message.connection.should == conn
+    message.message_id.should == 'random id'
+    message.command.should == 'hello'
+    message.params.should == { :foo => 'bar' }
+  end
+end
